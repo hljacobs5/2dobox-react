d2bd725aa0ea1f6be6296d08ad673b753aee340c
Haley Jacobs <haleyljacobs@gmail.com>
2018-06-04 11:06:51 -0600

    Added inputs and submit button

---
+++ src/Idea.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Idea.css';
+
+const Idea = (props) => {
+  <div className='ideaCard'>
+  <h3>Title: {props.title-input}</h3>
+  </div>
+}
+
+export default Idea;
